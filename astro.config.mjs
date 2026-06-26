9f996a0106edcd25792935a481f7f1495b21494f
Maria Nadal <marianadal.mnm@gmail.com>
2026-06-26 09:51:42 +0200

    comit 39847938746389

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://mndesignweb.es', 
+  site: 'https://descubre.mndesignweb.es', 
   integrations: [
     sitemap()
   ]
